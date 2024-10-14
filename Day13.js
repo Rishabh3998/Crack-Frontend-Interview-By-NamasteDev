e0fc280f5d5df286502a620afaea6f659396ad10
Rishabh Kumar <rsk1998200@gmail.com>
2024-10-14 21:12:14 +0530

    Crack the FE interview course finish

---
+++ Day13.js
@@ -0,0 +1,44 @@
+// Machine coding round questions
+
+// 1. Develop a comment widget
+//      - Comment box
+//      - Comment button
+//      - Comments list
+//      - Nested comments (like a thread where a conversation can start for a single comment)
+//      - Edit comment
+//      - Delete comment
+//      - Like comment
+
+// 2. TODO list app like JIRA
+//      - There will be multiple statuses like Todo, in progress, in review, etc
+//      - Card like UI will there for each task.
+//      - Edit list name like Todo => To pick up
+//      - Edit, Delete task
+//      - Drag & drop the task and the list as well
+
+// 3. Food ordering app
+//      - Integrate a GET api to fetch the data
+//      - Data present: Name, Menu, ETA, Description, Rating, tags (Cuisines) (Restaurant)
+//      - Responsiveness is a must
+//      - Search bar to search restaurant by name (Implement debouncing)
+//      - Sorting functionality for restaurants
+//      - Sorting will be on Name, Rating, ETA, and tag
+
+// 4. Email web app
+//      - First column (A left sidebar) should have - inbox, sent, spam
+//      - Second column next to sidebar - List of mails present in inbox, sent, spam
+//      - Selected email should be shown in the third and largest column
+//      - Read and Unread flag on the email
+//      - Persist state - use LocalStorage
+//      - Delete / Favorite email
+
+// 5. Sudoku game
+//      - Create a 9x9 grid
+//      - 3x3 grid is filled with some data
+//      - row by row or col by col we have to fill the empty data
+//      - Read about the rules
+
+// 6. 2 products comparison
+//      - Compare iphone 15 vs iphone 16
+//      - Responsiveness is a must
+//      - Add new product, Delete product
